1483c06e4bcef8b3d9c429818b220483d8e440cb
DrInnovatio <turing1723@gmail.com>
2020-08-13 21:40:08 +1000

    wiring up JSON web tokens.

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,6 +2,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const User = mongoose.model('User');
 const router = express.Router();
+const jwt = require('jsonwebtoken');
+
+
 
 
 router.post('/signup', async (req, res) => {
@@ -12,11 +15,12 @@ router.post('/signup', async (req, res) => {
     const user = new User({ email, password });
     await user.save();
 
+    const token = jwt.sign({ userId: user._id }, 'MY_SECRET_KEY');
     res.send('You made a post request');
   } catch (err) {
     return res.status(422).send(err.message);
   }
 });
 
-module.exports = router;
+module.exports = router; 
 
